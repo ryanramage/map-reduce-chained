5658c23d375c5ce3b74277916d644859e22648fe
Ryan Ramage <ryan.ramage@gmail.com>
2013-11-14 09:22:10 -0800

    Add the couch data to load

---
+++ test/assets/couch_data.js
@@ -0,0 +1,10 @@
+module.exports = {
+    docs: [
+          {_id: '1', user: 'ryan', price: 30,  type: 'sale'}
+        , {_id: '2', user: 'ryan', price: 40,  type: 'sale'}
+        , {_id: '3', user: 'ryan', price: 40,  type: 'sale'}
+        , {_id: '4', user: 'bob',  price: 140, type: 'sale'}
+        , {_id: '5', user: 'bob',  price: 40,  type: 'sale'}
+    ]
+}
+
