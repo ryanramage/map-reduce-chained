534e5a1002b3b037125371d2275ba908bf3e02c8
Ryan Ramage <ryan.ramage@gmail.com>
2013-11-14 09:16:55 -0800

    setup the couch db stuff for testing

--- test/couch.js
+++ test/couch.js
@@ -1,39 +1,62 @@
 var chained_map_reduce = require('..'),
     rimraf = require('rimraf'),
-    numpad = require('numpad'),s
+    async = require('async'),
+    couchr = require('couchr'),
+    numpad = require('numpad'),
+    debounce = require('lodash.debounce'),
+    couch_data = require('./assets/couch_data'),
     t = rimraf.sync('/tmp/map-reduce-example'),
     db = require('level-sublevel')(require('level')('/tmp/map-reduce-example')),
-    nil_map = function(key, value, emit){},
-    assert = require('assert');
+    assert = require('assert'),
+    chains;
 
-var chains = chained_map_reduce(db, [
-    {
-        url: 'http://localhost:5984/smalls',
-        transform: function(row, emit){
-            delete row.doc._id;
-            delete row.doc._rev;
-            emit(row.id, JSON.stringify(row.doc));
-        },
-        name: 'sales',
-        map: function(key, value, emit) {
-            var doc = JSON.parse(value);
-            if (doc.type === 'sale')
-                emit(doc.user, doc.price);
-        },
-        reduce: function (acc, v) {
-          return Number(acc || 0) + Number(v)
-        }
+
+// just some setup to create the couch, and load it with the test data
+async.series([
+    function(cb){
+        couchr.del('http://localhost:5984/sales', function() {
+            cb(); // just ignore errors on the delete, might not exist
+        });
     },
-    {
-        name: 'top_sales',
-        map: function(key, value, emit) {
-            emit(numpad(value, 3), key);
+    async.apply( couchr.put, 'http://localhost:5984/sales'),
+    async.apply( couchr.post,'http://localhost:5984/sales/_bulk_docs', couch_data),
+    makeChains,
+], function(err, data) {
+
+    // wait till all the reduces are done before getting the final answer
+    var after_reduce = debounce(test_chains, 200);
+    chains[0].db._mappedIndexes['sales'].on('reduce', after_reduce);
+})
+
+
+function makeChains(cb) {
+    chains = chained_map_reduce(db, [
+        {
+            name: 'sales',
+            url: 'http://localhost:5984/sales',
+            transform: function(row, emit){
+                delete row.doc._id;
+                delete row.doc._rev;
+                emit(row.id, JSON.stringify(row.doc));
+            },
+            map: function(key, value, emit) {
+                var doc = JSON.parse(value);
+                if (doc.type === 'sale')
+                    emit(doc.user, doc.price);
+            },
+            reduce: function (acc, v) {
+              return Number(acc || 0) + Number(v)
+            }
+        },
+        {
+            name: 'top_sales',
+            map: function(key, value, emit) {
+                emit(numpad(value, 3), key);
+            }
         }
-    }
-]);
-
-
-setTimeout(test_chains, 3000);
+    ]);
+    cb();
+}
 
 
 function test_chains(){
